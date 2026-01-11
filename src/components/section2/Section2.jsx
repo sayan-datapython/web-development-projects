ec8c0f0c74fb898afa7f269cf7ea09e20858856c
sayan-datapython <sc5921353@gmail.com>
2026-01-11 19:55:24 +0530

    Add files via upload

---
+++ src/components/section2/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className='h-screen w-full bg-blue-800'>
+          This is second section
+    </div>
+  )
+}
+
+export default Section2
